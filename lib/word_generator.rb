28d66289823e1e08910bbef4bea1cf52c437976e
bkdir <direkburak1@gmail.com>
2018-09-03 13:47:33 -0400

    Initial Commit

---
+++ lib/word_generator.rb
@@ -0,0 +1,5 @@
+require "word_generator/version"
+
+module WordGenerator
+  # Your code goes here...
+end
---
+++ spec/word_generator_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe WordGenerator do
+  it "has a version number" do
+    expect(WordGenerator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
