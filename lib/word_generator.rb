67eb877556e67b6eb2754903c7f7873bac83411d
bkdir <direkburak1@gmail.com>
2018-09-09 04:40:51 -0400

    Clean up configuration settings

--- lib/word_generator.rb
+++ lib/word_generator.rb
@@ -1,12 +1,10 @@
-require "word_generator/configuration"
 require "word_generator/version"
+require "word_generator/configuration"
 require "word_generator/word"
 require "word_generator/word_list"
 require "word_generator/generator"
 
 module WordGenerator
-  MAX_WORD_LENGTH = 9
-
   class << self
     attr_accessor :configuration
   end
--- lib/word_generator/configuration.rb
+++ lib/word_generator/configuration.rb
@@ -1,9 +1,13 @@
 module WordGenerator
   class Configuration
-    attr_accessor :max_word_length
+    MAX_WORD_LENGTH = 9
+    DATA_SOURCE = :file
+
+    attr_accessor :max_word_length, :data_source
 
     def initialize
-      @max_word_length = WordGenerator::MAX_WORD_LENGTH 
+      @max_word_length = MAX_WORD_LENGTH
+      @data_source = DATA_SOURCE
     end
   end
 end
