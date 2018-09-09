65d953ea940a337e805b7473fecfbf14c7d8f672
bkdir <direkburak1@gmail.com>
2018-09-08 20:21:59 -0400

    Working client for testing

--- lib/client.rb
+++ lib/client.rb
@@ -6,12 +6,17 @@ while true do
 
   letter_list = gets.chomp.split("")
   max_size = WordGenerator::MAX_WORD_LENGTH
+
   if letter_list.size > max_size
     puts "Input can't be longer than #{max_size} letters"
     next
   end
+
   generator = WordGenerator::Generator.new(letter_list)
-  word_list = generator.generate
+
+  word_list = (3..letter_list.size).each_with_object({}) do |word_length, result|
+    result[word_length] = generator.generate(word_length)
+  end
 
   word_list.each do |key, val|
     puts "#{key} letters ---> #{val.join(", ")}"
